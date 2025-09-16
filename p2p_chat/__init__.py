d75e14061d758daa932bc24ae42a15aee24a912e
Andre <andre@nothof.cloud>
2025-09-16 22:18:40 +0200

    Update __init__.py

--- p2p_chat/__init__.py
+++ p2p_chat/__init__.py
@@ -5,5 +5,5 @@ A secure peer-to-peer chat application with end-to-end encryption
 and a beautiful modern GUI powered by CustomTkinter.
 """
 
-__version__ = "2.0.0"
-__author__ = "P2P Chat Team" 
+__version__ = "1.0.0"
+__author__ = "Andre Nothof" 
